e91fb9d03f0a9fa0b2013eef78470dd6766a5e3f
Leo <lnddru@gmail.com>
2019-11-17 10:55:52 -0400

    Add files via upload

---
+++ src/app/api.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor( private httpClient: HttpClient) { }
+
+
+  getPosts(page){
+  		return this.httpClient.get(`https://reqres.in/api/users?page=${page}`);
+  	}
+
+  sendPostRequest(postData){
+
+  		const httpOpitions = {
+  			headers: new HttpHeaders({
+  				'Accept': 'application/json',
+  				'Content-Type': 'application/json'
+  			}),
+  		}
+
+  		return this.httpClient.post("https://reqres.in/api/users", postData, httpOpitions);
+  	}
+
+
+}
---
+++ src/app/sobre/sobre.page.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-sobre',
+  templateUrl: './sobre.page.html',
+  styleUrls: ['./sobre.page.scss'],
+})
+export class SobrePage implements OnInit {
+
+	
+
+  constructor(public alertController: AlertController) { }
+
+  ngOnInit() {
+  }
+  
+  async presentAlert(){
+
+    const alert = await this.alertController.create({
+      header: 'Alerta!',
+      subHeader: 'Mensagem Enviada',
+      message: 'Mensagem enviada com sucesso , logo mais entraremos em contato.',
+      buttons: ['OK']
+
+    });
+
+    await alert.present();
+    let result = await alert.onDidDismiss();
+    console.log(result);
+
+
+  }
+
+}
